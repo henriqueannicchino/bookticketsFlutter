23a95d9be110dd68f5f69e39165e649a19ebb8db
henriqueannicchino <henriquematos285@gmail.com>
2025-03-23 20:01:08 +0100

    feat: added scroll ticket

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -85,7 +85,16 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           const Gap(15),
-          TicketView(),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: [
+                TicketView(),
+                TicketView(),
+              ],
+            ),
+          )
         ],
       ),
     );
